5df34f715381611642d3cb3f2e164001b93e6095
[wanwang2] <632468635@qq.com>
2016-11-29 16:25:55 +0800

    vv

--- wanwan-javacompile/src/main/java/org/wanwanframework/javacompile/symbols/Env.java
+++ wanwan-javacompile/src/main/java/org/wanwanframework/javacompile/symbols/Env.java
@@ -5,13 +5,25 @@ import java.util.Hashtable;
 import org.wanwanframework.javacompile.inter.Id;
 import org.wanwanframework.javacompile.lexer.Token;
 
+/**
+ * 符号表
+ * @author coco
+ *
+ */
 public class Env {
 
 	private Hashtable<Object, Object> table;
-	protected Env pre;
-	public Env(Env n) { table = new Hashtable<>(); pre = n;}
+	protected Env prev;
+	
+	public Env(Env n) { table = new Hashtable<>(); prev = n;}
+	
 	public void put(Token w, Id i) {table.put(w, i);}
+	
 	public Id get(Token w) {
+		for (Env e = this ; e != null; e = e.prev) {
+			Id found = (Id)(e.table.get(w));
+			if(found != null) return found;
+		}
 		return null;
 	}
 }
