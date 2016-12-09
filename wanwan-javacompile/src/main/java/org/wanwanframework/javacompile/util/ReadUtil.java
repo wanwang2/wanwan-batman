52fde351570a0835bf66e20840526f976f88be2e
[wanwang2] <632468635@qq.com>
2016-12-09 11:11:47 +0800

    v

--- wanwan-javacompile/src/main/java/org/wanwanframework/javacompile/util/ReadUtil.java
+++ wanwan-javacompile/src/main/java/org/wanwanframework/javacompile/util/ReadUtil.java
@@ -5,22 +5,40 @@ import org.wanwanframework.log.Log;
 public class ReadUtil {
 
 	public static String content = "";
-	public static int currentIndex = 0;
 	
+	private static int currentIndex = 0;
+
 	/**
 	 * 获得读取的字符
+	 * 
 	 * @return
 	 */
 	public static char readchar() {
-		char r = (char)content.charAt(currentIndex++);
+		char r = (char) content.charAt(currentIndex);
+		if (currentIndex < content.length()) {
+			currentIndex++;
+		}
 		return r;
 	}
-	
+
+	/**
+	 * 是否读取到字符末尾
+	 * 
+	 * @return
+	 */
+	public static boolean isEnd() {
+		if (currentIndex < content.length()) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 	public static void main(String[] args) {
 		ReadUtil.content = "abcdefg...abcdefg...";
-		for(int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			Log.log(ReadUtil.readchar());
 		}
-		
+
 	}
 }
